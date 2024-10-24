7ed65276253bc1b2dfff021bcfb01cdad43b3475
WalterGaldamezWeb <walter.galdamez.06@gmail.com>
2024-10-23 21:22:04 -0600

    Se implemento funcionalidad a caja de busqueda

--- src/app/gifs/componentes/caja-de-busqueda/caja-de-busqueda.component.ts
+++ src/app/gifs/componentes/caja-de-busqueda/caja-de-busqueda.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-caja-de-busqueda',
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class CajaDeBusquedaComponent {
 
+  @ViewChild('inputGif')
+  public inputGif!: ElementRef;
+
+  constructor(){}
+
+  buscarGif(){
+    this.inputGif.nativeElement.addEventListener('keyup', this.teclaPresionada.bind(this));
+  }
+
+  teclaPresionada(eventoTeclado: KeyboardEvent) {
+    if (eventoTeclado.key === 'Enter') {
+      console.log('Nombre Gif:', this.inputGif.nativeElement.value);
+      // Aquí puedes agregar la lógica que necesites al presionar Enter
+
+    }
+  }
 }
